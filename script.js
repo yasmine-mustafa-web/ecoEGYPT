a2448409dfcb2f864db827940c4e588a3962a6f6
Esraa <essomostafa57@gmail.com>
2026-02-23 18:53:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = document.querySelectorAll('.btnQ');
+
+questions.forEach(q => {
+    q.addEventListener('click', () => {
+
+        const btnA = q.nextElementSibling;
+
+        const isOpen = btnA.classList.contains('show');
+
+        document.querySelectorAll('.').forEach(answer => {
+            answer.classList.remove('show');
+        });
+
+        if (!isOpen) {
+            btnA.classList.add('show');
+        }
+    });
+});
+const menuBtn = document.getElementById('menu');
+const ul = document.getElementById('ul');
+
+menuBtn.addEventListener('click', (e) => {
+    ul.classList.toggle('show');
+    e.stopPropagation();
+});
+document.addEventListener('click', (e) => {
+  if (!ul.contains(e.target) && !menuBtn.contains(e.target)) {
+    ul.classList.remove('show');
+  }
+});
